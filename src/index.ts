494684c3a88bd885bc907cf22ef536ace898cc70
ytakahashi <yt01tc@gmail.com>
2020-11-05 07:02:32 +0900

    prettier

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,7 @@ import * as fs from 'fs'
 import Database from 'better-sqlite3'
 
 class History {
-  constructor(
-    public readonly title: string,
-    public readonly url: string
-  ){}
+  constructor(public readonly title: string, public readonly url: string) {}
 }
 
 const file = `${os.homedir()}/Library/Application Support/Google/Chrome/Default/History`
@@ -16,13 +13,14 @@ const dbFile = `${__dirname}/../db/history.db`
 fs.copyFileSync(file, dbFile)
 
 const options = { readonly: true, fileMustExist: true }
-const db = new Database(dbFile, options);
+const db = new Database(dbFile, options)
 
 export const readHistory = (): Array<History> => {
-  const query = 'SELECT url, title FROM urls ORDER BY last_visit_time DESC LIMIT 10'
+  const query =
+    'SELECT url, title FROM urls ORDER BY last_visit_time DESC LIMIT 10'
   const statement = db.prepare(query)
 
-  return statement.all().map(r => new History(r.title, r.url))
+  return statement.all().map((r) => new History(r.title, r.url))
 }
 
 console.log(readHistory())
